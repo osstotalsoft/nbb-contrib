9507844aeffc374ca27028aae943df3c464902e6
dsimionescu@totalsoft.ro <dsimionescu@totalsoft.ro>
2022-03-18 10:19:39 +0200

    added tests

---
+++ test/Integration/NBB.Contrib.Sample.IntegrationTests/IntegrationSampleTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace NBB.Contrib.Sample.IntegrationTests
+{
+    public class IntegrationSampleTests
+    {
+        public IntegrationSampleTests()
+        {
+        }
+
+        [Fact]
+        public void Integration_sample_test()
+        {
+            //Arrange
+            var x = 1;
+
+            //Act
+            x++;
+            //Assert
+            x.Should().Be(2);
+        }
+    }
+}
---
+++ test/UnitTests/NBB.Contrib.Sample.Tests/SampleTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace NBB.Contrib.Sample.Tests
+{
+    public class SampleTests
+    {
+
+        public SampleTests()
+        {
+        }
+
+        [Fact]
+        public void Sample_test()
+        {
+            //Arrange
+            var x = 1;
+
+            //Act
+            x++;
+            //Assert
+            x.Should().Be(2);
+        }
+    }
+}
